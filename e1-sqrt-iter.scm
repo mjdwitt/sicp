409b9a0ca73a53dc80d8c045b3eddd942d729ba3
Michael DeWitt <md@hackfoolery.com>
2011-03-29 00:49:02 -0400

    Created file and added header comments

---
+++ e1-sqrt-iter.scm
@@ -0,0 +1,10 @@
+;; Michael DeWitt
+;; 29 March 2011
+;; SICP 1.1.7 Example: Square Roots by Newton's Method
+;;	Following along with the example in the book for using 
+;;	Newton's Method to approximate square roots.  First
+;;	(useful) exercise in recursion in this book.
+
+;;
+;; Procedure declarations
+;;
