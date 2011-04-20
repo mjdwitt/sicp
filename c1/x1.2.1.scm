f9903d52726573f7f2ee730874a15d745f2e5441
Michael DeWitt <md@hackfoolery.com>
2011-04-20 16:21:22 -0400

    added header comments

--- c1/x1.2.1.scm
+++ c1/x1.2.1.scm
@@ -1,5 +1,7 @@
+;; SICP Example from Fig. 1.3, Section 1.2.1
+;; Michael DeWitt
+;; 20 Aprile 2011
+
 (define (factorial x)
   (cond ((or (= x 0) (= x 1)) 1)
 		((> x 1) (* x (factorial (- x 1))))))
-
-
