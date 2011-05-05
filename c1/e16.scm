0f19defa8fc3cc2bdebb74821929fd6793023daf
Michael DeWitt <md@hackfoolery.com>
2011-05-05 09:42:20 -0400

    definitely spent waaaay too much time overthinking this one

--- c1/e16.scm
+++ c1/e16.scm
@@ -4,4 +4,13 @@
 ;;	Design an iterative procedure to calculate exponents
 ;;	in a logarithmic number of steps.
 
+(define (^ b n)
+  (define (odd? n)
+	(= (modulo n 2) 1))
+  (define (iter a b n)
+	(cond ((= n 0) a)
+		  ((odd? n) (iter (* a b) b (- n 1)))
+		  (else (iter a (* b b) (/ n 2)))))
+  (iter 1 b n))
 
+(display (^ 2 100)) (newline)
