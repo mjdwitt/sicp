97a42457b3e46fae70d1cd18af76b4cacb3c1378
Michael DeWitt <md@hackfoolery.com>
2011-05-06 15:02:22 -0400

    skeleton code from text plus a couple tests

--- c1/e19.scm
+++ c1/e19.scm
@@ -3,10 +3,21 @@
 ;; SICP Exercise 1.19
 
 (define (fib n)
+  (define (even? x)
+	(= (modulo x 2) 0))
   (define (iter a b n)
 	(cond ((= n 0) b)
-		  (else (iter (+ a b) a (- n 1)))))
-  (iter 1 0 n))
+		  ((even? n) (iter a
+						   b
+						   ;p'
+						   ;q'
+						   (/ n 2)))
+		  (else (iter (+ (* b q) (* a q) (* a p))
+					  (+ (* b p) (* a q))
+					  p
+					  q
+					  (- n 1)))))
+  (iter 1 0 0 1 n))
 
 (display (fib 9)) (newline)
 (display (fib 100)) (newline)
