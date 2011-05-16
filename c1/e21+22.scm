84857640a829e8c1284bf69aeba2a10f61fad24f
Michael DeWitt <md@hackfoolery.com>
2011-05-16 10:28:16 -0400

    Using larger values for n than suggested by the book and (process-time-clock) instead of (runtime) produces values that support the hypothesis that (prime? <val>) has a time complexity of sqrt(n).

--- c1/e21+22.scm
+++ c1/e21+22.scm
@@ -64,11 +64,11 @@
     ; modified from text to return boolean values indicating the
     ; primality of n
     (if (prime? n)
-      (report (- (runtime) time1))
+      (report (- (process-time-clock) time1))
       #f))
   (newline)
   (display n)
-  (start n (runtime)))
+  (start n (process-time-clock)))
 
 ;;	Using this procedure, write a procedure for finding the next
 ;;	three primes following a given whole number.
@@ -91,3 +91,18 @@
 (search-for-primes 10000)
 (search-for-primes 100000)
 (search-for-primes 1000000)
+
+;;	Given that (runtime) only measures system time in seconds, I 
+;;	rewrote the above to use the mit-scheme primitive (process-
+;;	time-clock), which measures in ticks.  Since ticks are
+;;	currently defined as only milliseconds, they still lack the
+;;	necessary resolution to form a conclusion only using 100,000
+;;	and 1,000,000 as values for n in search-for-primes.  Perhaps 
+;;	larger values of n will work?  Let's try.
+
+(search-for-primes 1000000000000)
+(search-for-primes 10000000000000)
+
+;;	Using larger values for n and the (process-time-clock) primitive,
+;;	it becomes clear that (prime? <val>) has a time complexity of
+;;	O(sqrt(n).
