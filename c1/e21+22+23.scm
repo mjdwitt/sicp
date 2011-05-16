17d6680391780c565ab66acfda887ae147ef85c9
Michael DeWitt <md@hackfoolery.com>
2011-05-16 10:40:09 -0400

    cleaned up some output in 1.21's header

--- c1/e21+22+23.scm
+++ c1/e21+22+23.scm
@@ -23,7 +23,7 @@
   (= n (smallest-divisor n)))
 
 
-(display "1.21:") (newline)
+(newline) (display "1.21:") (newline)
 ;;	Use the smallest-divisor procedure to find the smallest
 ;;	divisor of each of the following numbers: 199, 1999,
 ;;	and 19999.
