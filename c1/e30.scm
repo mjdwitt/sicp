6f17eb723d0a17cd42ba088537bc2ca15beb9fd1
Michael DeWitt <md@hackfoolery.com>
2011-05-27 09:53:05 -0400

    Implementation as written last night while reading before sleeping.

---
+++ c1/e30.scm
@@ -0,0 +1,12 @@
+;; Michael DeWitt
+;; May 26 2011
+;; SICP Exercise 1.30:
+;;	Rewrite the sum procedure implemented in this section
+;;	as an iterative process.
+
+(define (sum term a next b)
+  (define (iter a result)
+	(if (> a b)
+	  result
+	  (iter (next a) (+ (term a) result))))
+  (iter a 0))
