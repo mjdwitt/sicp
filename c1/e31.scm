b967090a45179967cb7ef39bfb49ff91347a14c2
Michael DeWitt <md@hackfoolery.com>
2011-05-27 10:01:19 -0400

    Implementation of product as written last night and skeleton code for appproximating pi using product.

---
+++ c1/e31.scm
@@ -0,0 +1,21 @@
+;; Michael DeWitt
+;; 26 May 2011
+;; SICP Exercise 1.31:
+;;	Write an abstraction similar to sum which multiplies a
+;;	range of terms in a sequence.
+
+(define (product term a next b)
+  (if (> a b)
+	1
+	(* (term a) (product term (next a) next b))))
+
+;;	Use this procedure to compute approximations to pi given
+;;	the formula that 
+;;		pi/4 = (2/3)(4/3)(4/5)(6/5)(6/7)(8/7)...
+
+(define (approximate-pi n)
+  ; Numerically approximates pi using the above formula, repeating
+  ; the multiplication to the nth term.
+  (define (term x) x)
+  (define (next x) x)
+  (* 4 (product term 2 next (+ n 1))))
