2765ab2f255adf024928abf40586be1b3a20ac37
Michael DeWitt <md@hackfoolery.com>
2011-06-07 23:12:16 -0400

    Completed part B.

--- c1/e37.scm
+++ c1/e37.scm
@@ -1,8 +1,8 @@
 ;; Michael DeWitt
 ;; 07 July 2011
 ;; SICP Exercise 1.37:
-;;	Write a function for computing continued fractions 
-;;	truncated to the kth term.
+;;	A.)	Write a function for computing continued fractions 
+;;		truncated to the kth term.
 
 (define (cont-frac N D k)
   ; computes the k-term finite continued fraction of
@@ -21,15 +21,37 @@
 
 
 
-;;	Now, use this procedure to compute 1/phi, where phi is
-;;	the golden ratio--(1 + sqrt 5)/2.
+;;		Now, use this procedure to compute 1/phi, where phi is
+;;		the golden ratio--(1 + sqrt 5)/2.
 
+(newline)
 (display "1/phi:\t")
 (display (cont-frac (lambda (x) 1.0)
 					(lambda (x) 1.0)
 					11)) (display "\t")
 (display (/ 2 (+ 1 (sqrt 5)))) (newline)
 
-;;	Computing the inverse of the golden ration as an 11th-
-;;	term continued fraction returns a result accurate to only
-;;	four digits.
+;;		Computing the inverse of the golden ration as an 11th-
+;;		term continued fraction returns a result accurate to only
+;;		four digits.
+
+
+
+;;	B.)	Implement and test a recursive version of the above 
+;;		procedure for computing continued fractions.
+
+
+;;		The recursive version:
+(define (rcont-frac N D k)
+  (define (recurse i)
+	(if (> i k)
+	  0
+	  (/ (N i) (+ (D i) (recurse (+ i 1))))))
+  (recurse 1))
+
+;;		And the test:
+(display "\t")
+(display (rcont-frac (lambda (x) 1.0)
+					 (lambda (x) 1.0)
+					 11)) (display "\t")
+(display (/ 2 (+ 1 (sqrt 5)))) (newline)
