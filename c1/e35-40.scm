8488718e37d374623cd3584af2f582f6932e9ea0
Michael DeWitt <md@hackfoolery.com>
2011-07-03 14:05:44 -0400

    Completed 1.43.

--- c1/e35-40.scm
+++ c1/e35-40.scm
@@ -233,3 +233,12 @@
 
 
 
+;; Exercise 1.43:
+;;	Write a procedure that takes as inputs a procedure that 
+;;	computes f and a positive integer n and returns the 
+;;	procedure that computes the nth repeated application of f.
+
+(define (repeated f n)
+  (if (= n 1)
+	f
+	(compose f (repeated f (- n 1)))))
