cbd6ececc5d7b8ea8321b6add84bf2bb7187784b
Michael DeWitt <md@hackfoolery.com>
2011-07-04 14:40:28 -0400

    Completed 1.46.
    Finished Chapter One! Last couple of problems need some comments added before merging to master and tagging, but all code is completed.

--- c1/e46.scm
+++ c1/e46.scm
@@ -7,4 +7,19 @@
 	  guess
 	  ((iter-improve done? improve) (improve guess)))))
 
+(define (sqrti x)
+  ((iter-improve
+	 (lambda (g)
+	   (< (abs (- (square g) x)) .00001))
+	 (lambda (g)
+	   (/ (+ g
+			 (/ x g))
+		  2)))
+   1.0))
 
+(define (fpi f guess)
+  ((iter-improve
+	 (lambda (g)
+	   (< (abs (- g (f g))) .00001))
+	 f)
+   guess))
